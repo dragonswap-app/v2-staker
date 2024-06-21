a92bd224b06434577f304bc6ba04b2cd75aadcd7
markolazic01 <markolazic01@gmail.com>
2024-06-21 19:36:07 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,1 @@
+yarn install && cd ./node_modules/@dragonswap/v2-core/ && yarn install --ignore-engines && yarn compile && cd ../v2-periphery/ && yarn install --ignore-engines && yarn compile && ../../../ && yarn compile
