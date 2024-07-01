9d367c655a9c5b056e35c18dc50df4f7455f224b
markolazic01 <markolazic01@gmail.com>
2024-07-01 16:35:12 +0200

    Workflow adaptations

--- install.sh
+++
@@ -1,1 +0,0 @@
-yarn install && cd ./node_modules/@dragonswap/v2-core/ && yarn install --ignore-engines && yarn compile && cd ../v2-periphery/ && yarn install --ignore-engines && yarn compile && ../../../ && yarn compile
