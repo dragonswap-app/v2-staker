d5619bcb6e61cb0f31733efab2f14b613f018735
milanmitic <miticm97@gmail.com>
2024-10-10 14:01:07 +0200

    chore: Added prettier write command

--- contracts/mocks/Tether.sol
+++ contracts/mocks/Tether.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Tether is ERC20 {
-    constructor() ERC20("Tether", "USDT") {
+    constructor() ERC20('Tether', 'USDT') {
         _mint(msg.sender, 100_000_000 * 10e6);
     }
 
