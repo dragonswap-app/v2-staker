eef976f2a1e281dd6ceaa1ff73b8addc2d5b3123
milanmitic <miticm97@gmail.com>
2024-10-14 16:13:34 +0200

    chore: Ran prettier

--- contracts/mocks/Token.sol
+++ contracts/mocks/Token.sol
@@ -6,9 +6,13 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 contract Token is ERC20 {
     uint8 private tokenDecimals;
 
-    constructor(string memory _name, string memory _symbol, uint8 _decimals) ERC20(_name, _symbol) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol) {
         tokenDecimals = uint8(_decimals);
-        _mint(msg.sender, 100_000_000 * 10 ** _decimals);
+        _mint(msg.sender, 100_000_000 * 10**_decimals);
     }
 
     function decimals() public view virtual override returns (uint8) {
